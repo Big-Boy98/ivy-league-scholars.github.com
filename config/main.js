171bd94e0a87530a7c77563bab0a4f8e4fc8dcd4
xcvbhnjmk,l. <Saloh13@icloud.com>
2025-03-01 16:21:00 -0500

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "ivy-league-scholars"; // Change this to change the name of your website.
+var subtext = "cool games to play"; // set the subtext
 
 // more settings in main.css
 
